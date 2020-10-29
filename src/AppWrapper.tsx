44dbc637751d3b46f496877d7fd3f5c285516ae9
Nikolay Stoynov <arvigeus@gmail.com>
2020-10-29 21:41:25 +0200

    feat: add dark theme color

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { RecoilRoot } from "recoil";
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { ThemeProvider, CSSReset, theme } from "@chakra-ui/core";
 
 // import { TranslationProvider } from "my-i18n-lib"
 // import defaultStrings from "i18n/en-x-default"
@@ -8,7 +8,7 @@ import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 const AppWrapper: FC = ({ children }) => {
   return (
     <RecoilRoot>
-      <ThemeProvider>
+      <ThemeProvider theme={customTheme}>
         <CSSReset />
         {/* <TranslationProvider messages={defaultStrings}> */}
         {children}
@@ -16,6 +16,20 @@ const AppWrapper: FC = ({ children }) => {
       </ThemeProvider>
     </RecoilRoot>
   );
+};
+
+const customTheme = {
+  ...theme,
+  colors: {
+    ...theme.colors,
+    dark: {
+      100: "#ddd",
+      400: "#7b7b7b",
+      600: "#333",
+      700: "#252526",
+      800: "#1e1e1e",
+    },
+  },
 };
 
 export default AppWrapper;
